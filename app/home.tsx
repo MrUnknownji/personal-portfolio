2246d6765c90c32c0d4f4f72ed5992f5022d0d3e
MrUnknownji <sandeepkhati788@gmail.com>
2024-10-13 19:59:05 +0530

    Maintaining streak

--- app/home.tsx
+++ app/home.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="h-screen my-20 pt-20">
       <h1 className="text-4xl font-bold text-center mt-20">
-        Welcome to My Portfolio
+        Hello world! I am Sandeep.
       </h1>
     </div>
   );
