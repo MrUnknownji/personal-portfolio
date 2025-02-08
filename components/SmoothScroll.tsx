52902142f0f3adac4dae204af8f2c21cde890672
MrUnknownji <sandeepkhati788@gmail.com>
2025-02-08 18:42:53 +0530

    Smoother Scrolling

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -2,11 +2,6 @@
 import { useEffect } from "react";
 import Lenis from "lenis";
 
-const ANIMATION_DURATION = 1.2;
-const LERP_SMOOTH_SCROLL = 0.1;
-const WHEEL_MULTIPLIER = 1;
-const TOUCH_MULTIPLIER = 2;
-
 export default function SmoothScroll({
   children,
 }: {
@@ -14,13 +9,13 @@ export default function SmoothScroll({
 }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: ANIMATION_DURATION,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 1.8, // Increase for slower scrolling
+      easing: (t: number) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
       orientation: "vertical",
       gestureOrientation: "vertical",
-      lerp: LERP_SMOOTH_SCROLL,
-      wheelMultiplier: WHEEL_MULTIPLIER,
-      touchMultiplier: TOUCH_MULTIPLIER,
+      lerp: 0.05, // Reduced lerp for even smoother interpolation
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
       infinite: false,
     });
 
