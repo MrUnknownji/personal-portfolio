a8d2f3fd49b44d73e1696312e51e7a6eb9aa93db
MrUnknownG <sandeepkhati788@gmail.com>
2025-03-15 14:12:46 +0530

    Added image configs in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,25 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: ["placehold.co", "linkedin.com", "github.com", "twitter.com"],
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/profile_images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
         port: "",
         pathname: "/**",
       },
