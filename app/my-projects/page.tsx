01f85b39490478e9354a5d6c735a9844149edf1b
MrUnknownJi <sandeepkhati788@gmail.com>
2026-05-07 17:27:04 +0530

    fixed build issue

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -280,7 +280,9 @@ export default function MyProjects() {
         });
       });
 
-    return () => transition.kill();
+    return () => {
+      transition.kill();
+    };
   }, [targetProjectIds]);
 
   useEffect(() => {
